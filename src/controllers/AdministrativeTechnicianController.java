81b8020a83211eda19a66417b7c5cef353338a0b
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2024-12-05 13:52:01 -0300

    Adicionando classes de controllers

---
+++ src/controllers/AdministrativeTechnicianController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import database.DatabaseDAO;
+
+public class AdministrativeTechnicianController {
+    public static DatabaseDAO db = DatabaseDAO.getInstance();
+
+    public void createAdministrativeTechnician() {}
+
+    public void listAdministrativeTechnician() {}
+
+    public void deleteAdministrativeTechnician() {}
+
+    public void searchAdministrativeTechnician() {}
+}
---
+++ src/controllers/ProfessorController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import database.DatabaseDAO;
+
+public class ProfessorController {
+    public static DatabaseDAO db = DatabaseDAO.getInstance();
+
+    public void createProfessor() {}
+
+    public void listProfessor() {}
+
+    public void deleteProfessor() {}
+
+    public void searchProfessor() {}
+}
